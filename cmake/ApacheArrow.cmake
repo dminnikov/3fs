1812521afebc15530861e11a2dbd05d18af0ae91
dvminnikov <dvminnikov@mts.ru>
2026-02-10 17:05:22 +0300

    fix GIT_REPOSITORY

--- cmake/ApacheArrow.cmake
+++ cmake/ApacheArrow.cmake
@@ -21,7 +21,7 @@ include(ExternalProject)
 ExternalProject_Add(
     apache-arrow-cpp
     PREFIX ${PREFIX}
-    GIT_REPOSITORY https://github.com/apache/arrow.git
+    GIT_REPOSITORY https://{{ git_username }}:{{ git_password }}@gitlab.services.mts.ru/dataops/datafactory/arrow.git
     GIT_TAG b7d2f7ffca66c868bd2fce5b3749c6caa002a7f0
     GIT_SHALLOW ON
     GIT_PROGRESS ON
